1a2789c247c93bdf5df9b518d6ab120448adc3e4
Randriamifidy Bezama Marolahy <marolahy@gmail.com>
2018-05-14 16:56:35 +0300

    first commit

---
+++ src/main/java/com/bezama/portalserver/PortalServerApplication.java
@@ -0,0 +1,12 @@
+package com.bezama.portalserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalServerApplication.class, args);
+	}
+}
